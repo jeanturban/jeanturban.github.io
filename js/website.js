7af73782df1299c4a38cd4593646b902b60bff95
jeanturban <jeanalex23@gmail.com>
2014-08-28 14:19:52 -0700

    Updating smooth scrolling

--- js/website.js
+++ js/website.js
@@ -4,6 +4,45 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+
+//js to make smooth scrolling from icons
+//Example: <a href="#" class="btn btn-circle scroll-link " data-id="section name" />
+//data-id essentially the same as href but globally accepted (not just "a" tag)
+$(document).ready(function() {
+	// navigation click actions
+	$('.scroll-link').on('click', function(event){
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		scrollToID('#' + sectionID, 750);
+	});
+	// scroll to top action
+	$('.scroll-top').on('click', function(event) {
+		event.preventDefault();
+		$('html, body').animate({scrollTop:0}, 'slow');
+	});
+	// mobile nav toggle
+	$('#nav-toggle').on('click', function (event) {
+		event.preventDefault();
+		$('#main-nav').toggleClass("open");
+	});
+});
+// scroll function
+function scrollToID(id, speed){
+	var offSet = 50;
+	var targetOffset = $(id).offset().top - offSet;
+	var mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+	if (mainNav.hasClass("open")) {
+		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+		mainNav.removeClass("open");
+	}
+}
+if (typeof console === "undefined") {
+    console = {
+        log: function() { }
+    };
+}
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
